84ee868ec5b7413cc193e9bab8da38ada8acbc66
Jorge Fernandez-de-Cossio-Diaz <cossio@users.noreply.github.com>
2022-11-01 20:24:31 +0100

    LazyArtifacts in place of Artifacts

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,4 @@
-# try to make sure Artifacts are downloaded
-import LazyArtifacts
-
-module Inner
-
-using Artifacts, Test
+using Test
+using LazyArtifacts
 
 @test isfile(joinpath(artifact"PyData", "backRBM_A_02_01.hdf5"))
-
-end
