e76ed22008a2d5b548a0b9803673f7815f66b391
Jorge Fernandez-de-Cossio-Diaz <cossio@users.noreply.github.com>
2022-11-01 17:26:21 +0100

    use LazyArtifacts

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,5 @@
 # try to make sure Artifacts are downloaded
-import Pkg
-Pkg.instantiate()
-
+import LazyArtifacts
 using Artifacts, Test
 
 @test isfile(joinpath(artifact"PyData", "backRBM_A_02_01.hdf5"))
