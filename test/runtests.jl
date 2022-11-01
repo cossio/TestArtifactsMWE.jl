68219ed9395d6de5ac8135832eaee3f27580f72d
Jorge Fernandez-de-Cossio-Diaz <cossio@users.noreply.github.com>
2022-11-01 20:19:57 +0100

    inner module

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,10 @@
 # try to make sure Artifacts are downloaded
 import LazyArtifacts
+
+module Inner
+
 using Artifacts, Test
 
 @test isfile(joinpath(artifact"PyData", "backRBM_A_02_01.hdf5"))
+
+end
